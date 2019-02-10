315056563312344a2629d55ca24c5160a15a1d62
Łukasz Makuch <zopsesen@gmail.com>
2019-02-10 13:46:06 +0100

    Added the getDispatchedActions helper.

--- test/steps/click_button.js
+++ test/steps/click_button.js
@@ -1,14 +1,15 @@
 import { fireEvent } from 'snabbdom-testing-library';
 import { consumeActionsWithEffects } from 'rosmaro-testing-library';
 
-export default ({ text }) => ({ testContext }) => ({
+export default ({ text }) => ({
+  testContext: { getDispatchedActions, render }
+}) => ({
   feed: { type: 'RENDER' },
   consume: ({ result }) => {
-    testContext.store.clearActions();
-    const { getByText } = testContext.render(result.data);
+    const { getByText } = render(result.data);
     const buttonToClick = getByText(text);
     fireEvent.click(buttonToClick);
-    const actions = testContext.store.getActions();
+    const actions = getDispatchedActions();
     return { step: consumeActionsWithEffects(actions) };
   }
 });
--- test/utils/testFlow.js
+++ test/utils/testFlow.js
@@ -2,10 +2,20 @@ import makeRoot from '~/src/components/root';
 import { testFlow } from 'rosmaro-testing-library';
 import configureStore from 'redux-mock-store';
 import rosmaro from 'rosmaro';
-import render from '~/test/utils/render';
+import makeRender from '~/test/utils/render';
 
 export default flow => {
   let store = configureStore([])({});
   const model = rosmaro(makeRoot({ ...store }));
-  testFlow({ model, flow, initialTestContext: { render: render(), store } });
+  let getDispatchedActions = () => {
+    const actions = store.getActions();
+    store.clearActions();
+    return actions;
+  };
+  const render = makeRender();
+  testFlow({
+    model,
+    flow,
+    initialTestContext: { render, getDispatchedActions }
+  });
 };
