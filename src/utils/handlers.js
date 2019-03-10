ca90e761b383135ea80ef15170302338528120ea
Łukasz Makuch <zopsesen@gmail.com>
2019-03-10 16:21:50 +0100

    Updated deps.

--- src/utils/handlers.js
+++ src/utils/handlers.js
@@ -2,10 +2,9 @@ import {
   typeHandler,
   defaultHandler,
   partialReturns,
-  targetedActions,
-  supportEntryActions
+  targetedActions
 } from 'rosmaro-binding-utils';
 
-export const makeHandler = handlerPlan => supportEntryActions(targetedActions()(partialReturns(typeHandler({defaultHandler})(handlerPlan))));
+export const makeHandler = handlerPlan => targetedActions()(partialReturns(typeHandler({defaultHandler})(handlerPlan)));
 
 export const transparentHandler = makeHandler({});
