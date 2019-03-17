d53f1257b36ba4008ba95f8f82ae045a86a28d50
Łukasz Makuch <zopsesen@gmail.com>
2019-03-17 21:39:00 +0100

    Updated the test runner

--- test/utils/runner.js
+++ test/utils/runner.js
@@ -3,10 +3,11 @@ import {testFlow, makeTreeRunner} from 'rosmaro-testing-library';
 import configureStore from 'redux-mock-store';
 import rosmaro from 'rosmaro';
 import makeRender from '~/test/utils/render';
+import {triggerEntryActions} from 'rosmaro-binding-utils';
 
 export const flow = (label, flow) => test(label, () => {
   let store = configureStore([])({});
-  const model = rosmaro(makeRoot({ ...store }));
+  const model = triggerEntryActions(rosmaro(makeRoot({ ...store })));
   let getDispatchedActions = () => {
     const actions = store.getActions();
     store.clearActions();
@@ -15,7 +16,7 @@ export const flow = (label, flow) => test(label, () => {
   const render = makeRender();
   testFlow({
     model,
-    flow,
+    flow: [{feed: {type: 'ON_ENTRY'}}, ...flow],
     initialTestContext: { render, getDispatchedActions }
   });
 });
